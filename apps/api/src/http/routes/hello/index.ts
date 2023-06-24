d794969260f2be2dca245264f878cfb86da0be2c
kodiyak <mathewsto51@gmail.com>
2023-06-24 17:39:35 -0300

    feat

--- apps/api/src/http/routes/hello/index.ts
+++ apps/api/src/http/routes/hello/index.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { hello } from "./handlers";
 
 const routes = async (router: Router) => {
-  router.get("/", hello);
+  router.get("/:type/:imdbId", hello);
 };
 
 export default routes;
