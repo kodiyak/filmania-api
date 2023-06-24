2ff2510e2cf4261b40282a85457d534dd5db8be8
kodiyak <mathewsto51@gmail.com>
2023-06-24 17:35:09 -0300

    feat

--- apps/api/src/http/routes/hello/handlers.ts
+++ apps/api/src/http/routes/hello/handlers.ts
@@ -7,7 +7,14 @@ export async function hello(req: Request, res: Response) {
     // const data = await new ShowsApi().getShowByImdbId("tt8421350");
     // const warezIds = await makeWarezApi().getAllIds();
     // const data = await makeSyncMovieService().byImdbId("tt1630029");
-    const data = await makeSyncSerieService().byImdbId("tt21844706");
+    const { type = "movie", imdbId } = req.params;
+    // const data = await makeSyncSerieService().byImdbId("tt21844706");
+    let data: any;
+    if (type === "movie") {
+      data = await makeSyncSerieService().byImdbId(imdbId);
+    } else {
+      data = await makeSyncMovieService().byImdbId(imdbId);
+    }
     res.json({
       // warezIds: warezIds.length,
       data,
