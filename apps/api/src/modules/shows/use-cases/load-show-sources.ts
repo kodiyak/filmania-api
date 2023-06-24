f6f5d9985fa395240f8dc74826a1d561397a7e1c
kodiyak <mathewsto51@gmail.com>
2023-06-24 01:53:58 -0300

    feat: load movie sources

--- apps/api/src/modules/shows/use-cases/load-show-sources.ts
+++ apps/api/src/modules/shows/use-cases/load-show-sources.ts
@@ -2,7 +2,10 @@ import { DispatcherService } from "@/modules/shared";
 import { LoadShowSourcesQuery } from "../queries/load-show-sources.query";
 import { db } from "@/lib/db";
 import { ShowsApi } from "@/modules/shared/services/api/shows.api";
-import { IShowsEpisodeApi } from "@/modules/shared/services/contracts/shows.api.interfaces";
+import {
+  IShowsEpisodeApi,
+  IShowsMovieApi,
+} from "@/modules/shared/services/contracts/shows.api.interfaces";
 import { ApplicationService } from "@/modules/shared/services/app.service";
 import { Show } from "@prisma/client";
 
@@ -27,9 +30,99 @@ export class LoadShowSources {
       throw new Error("Season and episode are required");
     }
 
+    let sources: any;
+
+    if (show.type !== "movie") {
+      sources = await this.getSerieEpisodeSources({
+        showId: show.id,
+        imdbId: show.imdbId,
+        seasonNumber,
+        episodeNumber,
+      });
+    } else {
+      sources = await this.getMovieSources({
+        showId: show.id,
+        imdbId: show.imdbId,
+      });
+    }
+
+    return {
+      sources,
+    };
+  }
+
+  private async getMovieSources({
+    imdbId,
+    showId,
+  }: {
+    showId: string;
+    imdbId: string;
+  }) {
+    const showData = await this.showsApi
+      .getShowMovieByImdbId(imdbId)
+      .catch(() => {
+        throw new Error(`Cannot load sources for show "${imdbId}"`);
+      });
+
+    const sources = await Promise.all([
+      this.parseMovieInfrazSources(showId, showData),
+      this.parseMovieWarezSources(showId, showData),
+    ]).then((res) => {
+      return res.reduce((acc: any, val: any) => [...acc, ...val], []);
+    });
+
+    return sources;
+  }
+
+  private async parseMovieInfrazSources(
+    showId: string,
+    showData: IShowsMovieApi.Data
+  ) {
+    if (!showData.infraz) {
+      return [];
+    }
+
+    return [
+      {
+        quality: "1080p",
+        type: "native",
+        url: this.app.withApiUrl(`/api/stream/iz/${showId}.mp4`),
+        lang: "dub",
+      },
+    ];
+  }
+
+  private async parseMovieWarezSources(
+    showId: string,
+    episodeData: IShowsMovieApi.Data
+  ) {
+    if (!episodeData.warez) {
+      return [];
+    }
+
+    return episodeData.warez.sources.map((source) => ({
+      quality: "720p",
+      type: "embed",
+      url: source.url,
+      subtitle: source.subtitleUrl,
+      lang: source.lang,
+    }));
+  }
+
+  private async getSerieEpisodeSources({
+    episodeNumber,
+    imdbId,
+    seasonNumber,
+    showId,
+  }: {
+    showId: string;
+    imdbId: string;
+    seasonNumber: number;
+    episodeNumber: number;
+  }) {
     const season = await db.season.findFirstOrThrow({
       where: {
-        showId: show.id,
+        showId: showId,
         number: seasonNumber,
       },
       select: {
@@ -43,10 +136,10 @@ export class LoadShowSources {
     });
 
     const episodeData = await this.showsApi
-      .getShowEpisodeByImdbId(show.imdbId, seasonNumber, episodeNumber)
+      .getShowEpisodeByImdbId(imdbId, seasonNumber, episodeNumber)
       .catch(() => {
         throw new Error(
-          `Cannot load sources for show "${show.imdbId}" season ${seasonNumber} episode ${episodeNumber}`
+          `Cannot load sources for show "${imdbId}" season ${seasonNumber} episode ${episodeNumber}`
         );
       });
 
@@ -54,23 +147,21 @@ export class LoadShowSources {
 
     if (!episodeId) {
       throw new Error(
-        `Cannot load sources for show "${show.imdbId}" season ${seasonNumber} episode ${episodeNumber}`
+        `Cannot load sources for show "${imdbId}" season ${seasonNumber} episode ${episodeNumber}`
       );
     }
 
     const sources = await Promise.all([
-      this.parseInfrazSources(episodeId, episodeData),
-      this.parseWarezSources(episodeId, episodeData),
+      this.parseEpisodeInfrazSources(episodeId, episodeData),
+      this.parseEpisodeWarezSources(episodeId, episodeData),
     ]).then((res) => {
       return res.reduce((acc: any, val: any) => [...acc, ...val], []);
     });
 
-    return {
-      sources,
-    };
+    return sources;
   }
 
-  private async parseInfrazSources(
+  private async parseEpisodeInfrazSources(
     episodeId: string,
     episodeData: IShowsEpisodeApi.Data
   ) {
@@ -83,11 +174,12 @@ export class LoadShowSources {
         quality: "1080p",
         type: "native",
         url: this.app.withApiUrl(`/api/stream/iz/${episodeId}.mp4`),
+        lang: "dub",
       },
     ];
   }
 
-  private async parseWarezSources(
+  private async parseEpisodeWarezSources(
     episodeId: string,
     episodeData: IShowsEpisodeApi.Data
   ) {
@@ -100,6 +192,7 @@ export class LoadShowSources {
       type: "embed",
       url: source.url,
       subtitle: source.subtitleUrl,
+      lang: source.lang,
     }));
   }
 }
