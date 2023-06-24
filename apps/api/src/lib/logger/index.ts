c7dc348d95ccf303197c908058ca1fe93366225f
kodiyak <mathewsto51@gmail.com>
2023-06-23 22:41:47 -0300

    feat: deploy

--- apps/api/src/lib/logger/index.ts
+++ apps/api/src/lib/logger/index.ts
@@ -1,7 +1,12 @@
+import { envConfig } from "@/configs/env";
 import pino from "pino";
 
-export const logger = pino({
-  transport: {
-    target: "pino-pretty",
-  },
-});
+export const logger = pino(
+  envConfig.isDev
+    ? {
+        transport: {
+          target: "pino-pretty",
+        },
+      }
+    : undefined
+);
